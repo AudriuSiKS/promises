34b4d24d6a031d13bad8cf6d04ad7bfc12c7e59c
Audrius Siliunas <audriuss@wix.com>
2017-08-21 05:45:03 +0300

    refactor getJson to promise

--- js/promises.js
+++ js/promises.js
@@ -1,13 +1,15 @@
 function getJSON(url, callback) {
-	var xobj = new XMLHttpRequest();
-	    xobj.overrideMimeType("application/json");
-	xobj.open('GET', url, true);
-	xobj.onreadystatechange = function () {
-	      if (xobj.readyState == 4 && xobj.status == "200") {
-	        callback(JSON.parse(xobj.responseText));
-	      }
+	const request = new XMLHttpRequest();
+    request.overrideMimeType("application/json");
+	request.open('GET', url, true);
+
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == "200") {
+			callback(JSON.parse(request.responseText));
+		}
 	};
-	xobj.send(null);  
+
+	request.send(null);  
 }
 
 function report(message) {
@@ -17,42 +19,6 @@ function report(message) {
 	document.getElementById("myList").appendChild(node);
 }
 
-// ********************** 
+// *****************
 
-report('At the code start');
 
-const ninjaDelayedPromise = new Promise((resolve, reject) => {
-	report('ninjaDelayedPromise executor');
-	setTimeout(() => {
-		report(' Resolving ninjaDelayedPromise');
-		resolve('Hattori');
-	}, 500);
-})
-
-//ninjaDelayedPromise is in pending state
-
-if (ninjaDelayedPromise !== null) {
-	report('After creating ninjaDelayedPromise');
-}
-
-ninjaDelayedPromise.then(ninja => {
-	if (ninja === 'Hattori') {
-		report('ninjaDelayedPromise resolved with Hattori');
-	}
-});
-
-const ninjaImmediatePromise = new Promise((resolve, reject) => {
-	report('ninjaImmediatePromise executor. Immediate resolve.');
-	resolve('Yoshi'); //The promise is immedietely resolved
-});
-
-//ninjaImmediatePromise state is resolved
-
-ninjaImmediatePromise.then(ninja => {
-	if (ninja === 'Yoshi') {
-		//even is the promise is resolved, the callback is called after the code in current event loop step is executed.
-		report('ninjaImmediatePromise resolve handled with Yoshi')
-	}
-})
-
-report('At the code end');
